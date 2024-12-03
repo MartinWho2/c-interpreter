7f66c3f85b6417461e471751238aeb13612fd2d3
MartinWho2 <martin.crettol@gmail.com>
2024-12-04 01:08:21 +0900

    added README

--- test_programs/add.c
+++ test_programs/add.c
@@ -5,6 +5,9 @@ int add(int a, int b) {
 int main() {
     int x = 5;
     int y = 7;
+    printf("x = %d\n",x);
+    printf("y = %d\n",y);
     int result = add(x, y);
+    printf("x + y = %d\n",result);
     return result;
 }
